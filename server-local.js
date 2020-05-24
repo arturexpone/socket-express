94534b0baf9b02a5ca2a0e2db5b0a7f4f6e01661
Artur <arturexpone@yandex.ru>
2020-05-24 14:31:25 +0300

    Update server-local.js

--- server-local.js
+++ server-local.js
@@ -1,5 +1,57 @@
 'use strict';
 
-const app = require('./express/server');
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const mongoose = require('./mongo/mongodb');
+const AllMessages = mongoose.AllMessages();
 
-app.listen(3000, () => console.log('Local app listening on port 3000!'));
+
+mongoose.connect();
+
+const PORT = 8888;
+
+app.get('/port', (req, res) => { res.end(`port: ${PORT}`)});
+
+io.on('connection', socket => {
+
+    socket.on('login', data => {
+        console.log('login')
+        AllMessages.create({roomId: data.roomId, userName: data.userName, message: ' '}).then(createData => {
+            AllMessages.find()
+                .then(arrOfData => {
+                    return io.emit('set all data', arrOfData)
+                }).catch(err => console.log(err))
+        })
+    })
+
+    socket.on('set room', data => {
+        console.log('set room')
+        AllMessages.find()
+            .then(arrOfData => {
+                return io.emit('set all data', arrOfData)
+            }).catch(err => console.log(err))
+    })
+
+    socket.on('send message', data => {
+        console.log('send message')
+        AllMessages.create({
+            roomId: data.roomId,
+            userName: data.userName,
+            message: data.message,
+            currentDate: data.currentDate
+        }).then(createData => {
+            AllMessages.find()
+                .then(arrOfData => {
+                    return io.emit('set all data', arrOfData)
+                }).catch(err => console.log(err))
+        })
+    })
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected')
+    });
+});
+
+
+server.listen(PORT);
